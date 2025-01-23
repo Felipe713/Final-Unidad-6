bac0b2b3d972e1ad7b6868efb2cff80b772e3862
Felipe Valenzuela <74212825+Felipe713@users.noreply.github.com>
2025-01-23 16:08:09 -0300

    tercer commit

---
+++ frontend/src/main/resources/static/js/app.js
@@ -0,0 +1,18 @@
+console.log("corriendo")
+function showModal(e){
+
+    const libroTitulo = e.getAttribute('data-bookName')
+    const libroDescripcion = e.getAttribute('data-bookDescription')
+    const libroIsbn = e.getAttribute('data-bookIsbn')
+
+    document.getElementById('exampleModalLabel').textContent = libroTitulo
+    document.querySelector('.modal-body #descripcion-section').textContent = `Descripción: ${libroDescripcion}`
+    document.querySelector('.modal-body #isbn-section').innerHTML = `<strong>ISBN:</strong> ${libroIsbn}`
+
+    const theModal = document.querySelector("#modalDetallea")
+    const myModalAlternative = new bootstrap.Modal(theModal)
+
+    myModalAlternative.show()
+    console.log("El onclick funciona...")
+    // return myModalAlternative
+}
